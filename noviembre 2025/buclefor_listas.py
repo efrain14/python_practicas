501bf51c3cdb979898c1b7bb5f382de44e3efeaa
efrain14 <efrain14@gmail.com>
2025-12-28 00:55:16 -0400

    ultimo commit del dia 28122025

---
+++ noviembre 2025/buclefor_listas.py
@@ -0,0 +1,15 @@
+# utilice un bucle for para imprimir los numeros del 1 al 20 inclusive
+
+numeros = list(range(1,21))
+print(numeros)
+
+print("\n")
+
+# haga una lista de numeros del 1 al 1_000_001
+numeros2 = list(range(1, 1000000))
+print(numeros2)
+
+print("\n")
+
+for valor in range(1, 1000001):
+    print(valor)
---
+++ noviembre 2025/quiz.py
@@ -0,0 +1,3 @@
+list = [10, 20 ,30]
+list.extend(list)
+print(list.count(10))
