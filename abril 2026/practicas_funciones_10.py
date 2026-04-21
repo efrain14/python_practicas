fa5e71e67e25a1e38bfb14541f8e6ee984c76eb1
efrain14 <efrain14@gmail.com>
2026-04-20 22:46:18 -0400

    commit para completar el trakin del dia 20042026

---
+++ abril 2026/practicas_funciones_10.py
@@ -0,0 +1,20 @@
+"""PRACTICAS DE FUNCIONES  CHARLY CIMINO """
+# Definiciön de funciones
+
+
+def leer_entero_validado(mensaje, min 0 = float("
+int(input(f"{mensaje}:
+-while nx min or n > max:
+-n = int(input(f"ERROR. {mensaje}:
+return n
+informar si numero_es perfecto(numero) :
+if :
+-Inf"),
+max
+1)
+- = float("lnf")
+print(f"E1 numero {numero} es perfecto")
+numero {numero} NO es perfecto")
+# Programa principal
+num = leer entero validado("lngrese un- nümero natural" ,
+informar si numero es_perfecto(num)
