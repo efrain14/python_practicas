caee75e9c89c58eb7ef92557ee5f94d6ba7b7638
efrain14 <efrain14@gmail.com>
2026-06-02 22:27:06 -0400

    ultimo commit casa noche 02062026

--- Freecodecam/buil_a_travel_weather.py
+++ Freecodecam/buil_a_travel_weather.py
@@ -6,11 +6,11 @@ Objetivo: Cumplir con las historias de usuario a continuación y pasar todas las
 User Stories:
 
 1) Debes crear las siguientes variables:
-distance_mi (un número que representa la distancia a recorrer en millas)
-is_raining (un booleano que representa si el usuario está experimentando clima lluvioso actualmente)
-has_bike (un booleano que representa si el usuario tiene una bicicleta)
-has_car (un booleano que representa si el usuario tiene un coche)
-has_ride_share_app (un booleano que representa si el usuario tiene una app que le permite solicitar un viaje)
+distance_mi (un número que representa la distancia a recorrer en millas) valor = 5
+is_raining (un booleano que representa si el usuario está experimentando clima lluvioso actualmente) valor = True
+has_bike (un booleano que representa si el usuario tiene una bicicleta) valor = False
+has_car (un booleano que representa si el usuario tiene un coche) valor = True
+has_ride_share_app (un booleano que representa si el usuario tiene una app que le permite solicitar un viaje) valor = False
 2) Debes usar sentencias condicionales para determinar si es posible el desplazamiento basado en los valores de estas variables.
 3) Debes usar las sentencias if, elif y else para evaluar las categorías de distancia en orden ascendente.
 4) Si distance_mi es un valor falso:
@@ -33,5 +33,31 @@ has_ride_share_app = False
 
 if distance_mi == 0:
     print(False)
-    if distance_mi >= 1:
+elif distance_mi <= 1 and not is_raining:
+    print(True)
+elif 1 < distance_mi <= 6 and not is_raining  and has_bike:
+    print(True)
+elif distance_mi < 6 and has_car and has_ride_share_app:
+    print(True)
+else:
+    print(False)
+    
+print("\n"*3)
+
+if distance_mi <= 0:
+    print(False)
+elif distance_mi <= 1:
+    if not is_raining:
         print(True)
+    else:
+        print(False)
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
+else:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
